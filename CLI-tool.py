567e2c94f02e38f9b672f3b8fd845674f013a45f
stav shukrun <shukrunstav@gmail.com>
2021-06-03 16:04:50 +0300

    3th upgrade(sqlite)

--- CLI-tool.py
+++ CLI-tool.py
@@ -1,6 +1,7 @@
-from abc import abstractclassmethod, abstractmethod
+from abc import abstractmethod
 import click
 import json
+import sqlite3
 
 class DB:
     @abstractmethod
@@ -48,7 +49,7 @@ class JsonDB(DB):
         if key in self.data:
             click.echo(self.data[key]['value'])
         else: 
-            print("This key is not exictit")
+            raise KeyError(key)
         
     def set(self,key,value):
         self.data[key] = {"value": value, "outdated": False}
@@ -57,11 +58,11 @@ class JsonDB(DB):
         if key in self.data:
             del self.data[key]
         else:
-            print("This is not a key!")
+            raise KeyError(key)
 
     def reset(self,key):
         if key not in self.data:
-            print("This key is not exictit")
+            raise KeyError(key)
         
         self.data[key]['outdated'] = True
         print(key, 'is now outdated!')
@@ -72,6 +73,46 @@ class JsonDB(DB):
     def __enter__(self):
         return self
 
+class sqlDB(DB):
+
+    def __init__(self):
+            self.conn = sqlite3.connect('sqlite_db.sqlite')
+            self.cur=self.conn.cursor()
+            self.cur.execute("CREATE TABLE IF NOT EXISTS hash (key TEXT PRIMARY KEY, value TEXT, outdated INTEGER );")
+
+    def get(self,key):
+        item = self.cur.execute(f'SELECT value FROM hash WHERE key = "{key}";').fetchall()
+        if not item:
+            raise KeyError(key)
+        return print(item)
+
+    def set(self,key,value):
+        keys = self.cur.execute(f"SELECT key FROM hash WHERE key='{key}'").fetchall()
+        if keys:
+            self.cur.execute(f'UPDATE hash SET value = "{value}", outdated = 0  WHERE key="{key}";')
+        else:
+            self.cur.execute(f'INSERT INTO hash (key, value, outdated) VALUES ("{key}", "{value}", 0);')
+
+    def delete(self, key):
+        keys = self.cur.execute(f"SELECT key FROM hash WHERE key='{key}'").fetchall()
+        if keys:
+            self.cur.execute(f"DELETE FROM hash WHERE key = '{key}';")
+        else:
+            raise KeyError(key)
+
+    def reset(self, key):
+        keys = self.cur.execute(f"SELECT key FROM hash WHERE key='{key}'").fetchall()
+        if keys:
+            self.cur.execute(f'UPDATE hash SET outdated = 1 WHERE key="{key}";')
+            print(key, 'is now outdated!')
+
+    def __enter__(self):
+         return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.conn.commit()
+        self.conn.close()
+
 @click.group()
 def cli():
    pass
@@ -79,26 +120,30 @@ def cli():
 @cli.command()
 @click.argument('key')
 def get(key):
-    with JsonDB() as db:
+    with sqlDB() as db:
+    # with JsonDB() as db:
         db.get(key)
 
 @cli.command()
 @click.argument('key')
 @click.argument('value')
 def set(key,value):
-    with JsonDB() as db:
+    with sqlDB() as db:
+    # with JsonDB() as db:
         db.set(key,value)
    
 @cli.command()
 @click.argument('key')
 def delete(key):
-    with JsonDB() as db:
+    with sqlDB() as db:
+    # with JsonDB() as db:
         db.delete(key)
 
 @cli.command()
 @click.argument('key')
 def reset(key):
-    with JsonDB() as db:
+    with sqlDB() as db:
+    # with JsonDB() as db:
         db.reset(key)
 
 if __name__ == '__main__':
